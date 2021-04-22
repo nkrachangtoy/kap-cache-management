91556ec9dd44d27617de4bdc76de6729402afaab
russtelen <rtelen@my.bcit.ca>
2021-04-21 17:19:09 -0700

    [KKIP-73] Create initial KeysController.cs

---
+++ server/KONNECT-REDIS/Controllers/KeysController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KONNECT_REDIS.Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KONNECT_REDIS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KeysController : ControllerBase
+    {
+        private IKeysService _keysService;
+
+        public KeysController(IKeysService keysService)
+        {
+            _keysService = keysService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var res = _keysService.Test();
+
+            return Ok(res); 
+        }
+    }
+}
