dead41012e49de8a718488ee910459d1c7459694
russtelen <rtelen@my.bcit.ca>
2021-04-22 14:47:38 -0700

    [KKIP - 74] Refactor GetAllKeys in KeyService

--- server/KONNECT-REDIS/Services/KeysService.cs
+++ server/KONNECT-REDIS/Services/KeysService.cs
@@ -22,7 +22,7 @@ namespace KONNECT_REDIS.Services
         public ICollection<Key> GetAllKeys()
         {
             var keys = _multiplexer.GetServer("localhost", 6379).Keys();
-
+            
             var keyList = new List<Key>();
 
             foreach (var key in keys)
@@ -31,11 +31,12 @@ namespace KONNECT_REDIS.Services
                 // ex key: IsFeatureActive#ad_emit_events#1
                 // f1 === IsFeatureActive
                 // f2 === ad_emit_event
-                // f3 === 1
+                // f3? === 1
+                var keyString = key.ToString();
 
-                var f1 = "fdsf";
-                var f2 = "fdsfsdf";
-                var f3 = "fsdfsdf";
+                var f1 = keyString.Split("#")[0];
+                var f2 = keyString.Split("#")[1];
+                var f3 = keyString.Split("#")[2] != null ? keyString.Split("#")[2] : "";
 
                 var keyObj = new Key { Field1 = f1, Field2 = f2, Field3 = f3};
 
@@ -43,9 +44,8 @@ namespace KONNECT_REDIS.Services
             }
 
             return keyList
-                .OrderBy(k => k.Field1)
-                .ToList();
+                    .OrderBy(k => k.Field1)
+                    .ToList();
         }
-
     }
 }
