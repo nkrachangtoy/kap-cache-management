5961eae0c8ae7f9bb4b8d73c262274e30bec3456
russtelen <rtelen@my.bcit.ca>
2021-04-22 11:52:21 -0700

    [KKIP-74] Refactor Key model for dynamic filtering

--- server/KONNECT-REDIS/Models/Key.cs
+++ server/KONNECT-REDIS/Models/Key.cs
@@ -7,6 +7,8 @@ namespace KONNECT_REDIS.Models
 {
     public class Key
     {
-        public string Text { get; set; }
+        public string field1 { get; set; }
+        public string field2 { get; set; }
+        public string field3 { get; set; }
     }
 }
