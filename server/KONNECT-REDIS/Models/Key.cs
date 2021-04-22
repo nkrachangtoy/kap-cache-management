0867718e988acaab619ccdbeffc52e9be72f17a7
russtelen <rtelen@my.bcit.ca>
2021-04-22 11:40:52 -0700

    [KKIP-74] Create Key.cs model

---
+++ server/KONNECT-REDIS/Models/Key.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KONNECT_REDIS.Models
+{
+    public class Key
+    {
+        public string Text { get; set; }
+    }
+}
