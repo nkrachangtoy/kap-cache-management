4a5825b42baa2c27c524f24e1205e5359142a784
Sabrina Kuah <swkuah@gmail.com>
2021-04-27 23:00:55 -0700

    [KKIP-106] Add reset button

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -7,22 +7,25 @@ interface SearchProps {
 const Search: React.FC<SearchProps> = ({ handleSearch }) => {
   const [query, setQuery] = useState("");
   return (
-    <form
-      className="search"
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSearch(query);
-        setQuery("");
-      }}
-    >
-      <input
-        placeholder="Search with Redis pattern"
-        onChange={(e) => setQuery(e.target.value)}
-        type="text"
-        value={query}
-      />
-      <button type="submit">Search</button>
-    </form>
+    <>
+      <form
+        className="search"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSearch(query);
+          setQuery("");
+        }}
+      >
+        <input
+          placeholder="Search with Redis pattern"
+          onChange={(e) => setQuery(e.target.value)}
+          type="text"
+          value={query}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <button onClick={() => handleSearch("*")}>Reset</button>
+    </>
   );
 };
 
